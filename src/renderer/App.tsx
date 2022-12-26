5a4e09c0b84ee231c38b8dd869dab0aff6450395
kangju2000 <kangju2000@naver.com>
2022-12-27 00:48:19 +0900

    refactor: change folder structure and create store page - add store route - create useInputs hook

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,57 +1,17 @@
-import {
-  MemoryRouter as Router,
-  Routes,
-  Route,
-  useNavigate,
-} from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Bill from './screens/Bill';
-import routes from './routes';
-
-const Hello = () => {
-  const navigate = useNavigate();
-
-  const onClick = () => {
-    navigate(routes.bill);
-  };
-
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <button type="button" onClick={() => navigate(routes.bill)}>
-          <span role="img" aria-label="folded hands">
-            🙏
-          </span>
-          Donate
-        </button>
-      </div>
-    </div>
-  );
-};
+import './styles/common.scss';
+import Bill from './pages/BillPage/BillPage';
+import Home from './pages/HomePage/HomePage';
+import Store from './pages/StorePage/StorePage';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<Home />} />
         <Route path="/bill" element={<Bill />} />
+        <Route path="/store" element={<Store />} />
       </Routes>
     </Router>
   );
---
+++ src/renderer/types/index.ts
@@ -0,0 +1,36 @@
+export interface Bill {
+  store: Store;
+  memo?: string;
+  transactionDate?: Date;
+  orderProducts: OrderProduct[];
+}
+
+export interface Store {
+  businessNumber: number;
+  address: string;
+  name: string;
+  owner: string;
+  bills: Bill[];
+}
+
+export interface OrderProduct {
+  count: number;
+  transactionDate?: Date;
+  product: Product;
+  bill?: Bill;
+}
+
+export interface Product {
+  name: string;
+  price: number;
+  category: Category;
+  orderProducts: OrderProduct[];
+}
+
+export interface Category {
+  name: string;
+  level: number;
+  parentCategory: Category;
+  childCategories: Category[];
+  products: Product[];
+}
