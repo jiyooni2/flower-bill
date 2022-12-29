36dd9c4b727277c652bb7a9d50d18421a6190658
kangju2000 <kangju2000@naver.com>
2022-12-29 19:16:34 +0900

    feat: implement pagination on product list

--- src/renderer/pages/UserPage/UserPage.tsx
+++ src/renderer/pages/UserPage/UserPage.tsx
@@ -19,24 +19,11 @@ import styles from './UserPage.module.scss';
 const UserPage = () => {
   const [products, setProducts] = useRecoilState(productsState);
   const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
   const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const handleChangePage = (
-    event: React.MouseEvent<HTMLButtonElement> | null,
-    newPage: number
-  ) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  useEffect(() => {
+  const [currentMenu, setCurrentMenu] = useState<'product' | 'category'>(
+    'product'
+  );
+  -useEffect(() => {
     window.electron.ipcRenderer.sendMessage('get-products', {});
     window.electron.ipcRenderer.on(
       'get-products',
@@ -50,52 +37,68 @@ const UserPage = () => {
     <>
       <CreateProductModal isOpen={isOpen} setIsOpen={setIsOpen} />
       <div className={styles.content_container}>
-        <p>상품 목록</p>
+        <Button variant="contained" onClick={() => setCurrentMenu('product')}>
+          상품 목록
+        </Button>
+        <Button variant="contained" onClick={() => setCurrentMenu('category')}>
+          카테고리 목록
+        </Button>
         <div className={styles.content}>
-          <Button onClick={() => setIsOpen(true)}>상품 추가</Button>
-          <Table stickyHeader>
-            <TableHead>
-              <TableRow>
-                <TableCell align="center">이름</TableCell>
-                <TableCell align="center">카테고리</TableCell>
-                <TableCell align="center">가격</TableCell>
-                <TableCell align="center">설정</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {products.map((product) => (
-                <TableRow key={product.id}>
-                  <TableCell align="center">{product.name}</TableCell>
-                  <TableCell align="center">{product.categoryId}</TableCell>
-                  <TableCell align="center">
-                    {product.price.toLocaleString('ko-KR')}원
-                  </TableCell>
-                  <TableCell>
-                    <Button>수정</Button>
-                    <Button>삭제</Button>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-            <TableFooter>
-              <TableRow>
-                <TablePagination
-                  rowsPerPageOptions={[5, 10, 25]}
-                  count={products.length}
-                  page={page}
-                  rowsPerPage={rowsPerPage}
-                  onPageChange={handleChangePage}
-                  onRowsPerPageChange={handleChangeRowsPerPage}
-                />
-              </TableRow>
-            </TableFooter>
-          </Table>
+          {currentMenu === 'product' ? (
+            <>
+              <Button onClick={() => setIsOpen(true)}>상품 추가</Button>
+              <Table stickyHeader>
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center">이름</TableCell>
+                    <TableCell align="center">카테고리</TableCell>
+                    <TableCell align="center">가격</TableCell>
+                    <TableCell align="center">설정</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {products.slice(page * 5, page * 5 + 5).map((product) => (
+                    <TableRow key={product.id}>
+                      <TableCell align="center">{product.name}</TableCell>
+                      <TableCell align="center">{product.categoryId}</TableCell>
+                      <TableCell align="center">
+                        {product.price.toLocaleString('ko-KR')}원
+                      </TableCell>
+                      <TableCell>
+                        <Button>수정</Button>
+                        <Button>삭제</Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+              <div className={styles.page_control}>
+                <Button
+                  onClick={() => {
+                    if (page > 0) setPage((prev) => prev - 1);
+                  }}
+                >
+                  &lt;
+                </Button>
+                {page + 1}
+                <Button
+                  onClick={() => {
+                    if (page < products.length % 5) setPage((prev) => prev + 1);
+                  }}
+                >
+                  &gt;
+                </Button>
+              </div>
+            </>
+          ) : (
+            <div>카테고리 목록</div>
+          )}
         </div>
       </div>
-      <div className={styles.content_container}>
+      {/* <div className={styles.content_container}>
         <p>카테고리 목록</p>
         <div className={styles.content} />
-      </div>
+      </div> */}
     </>
   );
 };
