482154b3e471339ddc2e1e8f16bcda286223c3ea
hyejinKim <jin031111@naver.com>
2023-01-31 10:07:40 +0900

    컴포넌트 삭제 및 페이지 업데이트

--- src/renderer/pages/SellerPage/SellerPage.tsx
+++ src/renderer/pages/SellerPage/SellerPage.tsx
@@ -1,34 +1,102 @@
+import { ChangeEvent, useState } from 'react'
 import Button from '@mui/material/Button';
-import SearchLayout from 'renderer/components/SearchLayout/SearchLayout';
 import Input from 'renderer/components/Input/Input';
 import styles from './SellerPage.module.scss';
 
-
 const SellerPage = () => {
-  const titleData = [
-    {title: '이름', titleEn: 'name'},
-    {title: '사업자 번호', titleEn: 'storeNumber'},
-    {title: '소유자 이름', titleEn: 'storeName'},
-    {title: '주소', titleEn: 'adress'},
-  ]
+  const USERS = [
+    {storeName: 'abc', name: '홍길동1', storeNumber: '123', owner: '홍길동1', address: '마장동'},
+    {storeName: 'def', name: '홍길동2', storeNumber: '456', owner: '홍길동2', address: '강동구'},
+    {storeName: 'ghi', name: '홍길동3', storeNumber: '789', owner: '홍길동3', address: '군포시'},
+  ];
+
+  const [name, setName] = useState<string>("");
+  const [foundUsers, setFoundUsers] = useState(USERS);
+  const [storeInfo, setStoreInfo] = useState([
+    {name: '', storeNumber: '', owner: '', address: ''},
+  ]);
+
+  const filter = (e: ChangeEvent<HTMLInputElement>) => {
+    const keyword = e.target.value;
+
+    if (keyword !== '') {
+      const results = USERS.filter((user) => {
+        return user.storeName.toLowerCase().startsWith(keyword.toLowerCase());
+      });
+      setFoundUsers(results);
+    } else {
+      setFoundUsers(USERS);
+    }
+
+    setName(keyword);
+  };
+
+
+  const changeDataHandler = (e:React.MouseEvent<HTMLElement>) => {
+    if (!(e.target instanceof HTMLButtonElement)) {
+      return;
+    }
+    const dataName = e.target.dataset['name'];
+
+    USERS.forEach((item) => {
+      if(item.storeName === dataName) {
+        setStoreInfo(
+          [{ name: item.name, storeNumber: item.storeNumber , owner: item.owner, address: item.address}],
+        );
+      }
+    });
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <div>
-          <SearchLayout />
+        <div className="searchInputs">
+        <input
+          type="search"
+          value={name}
+          onChange={filter}
+          placeholder="Filter"
+        />
+        <div className="user-list">
+        {foundUsers && foundUsers.length > 0 ? (
+          foundUsers.map(({storeName}, key) => (
+            <li key={key}>
+              <button onClick={changeDataHandler} data-name={storeName}>
+                {storeName}
+              </button>
+            </li>
+          ))
+        ) : (
+          <h1>검색 결과가 없습니다</h1>
+        )}
+        </div>
         </div>
         <div>
           <div className={styles.infoContent}>
             <h2>판매처 정보</h2>
             <div className={styles.list}>
-              {
-                titleData.map(({title, titleEn}, key) => (
-                  <div className={styles.item} key={key}>
-                    <p>{title}</p>
-                    <Input inputName={titleEn}></Input>
+              <div className={styles.item}>
+                {storeInfo.map(({name,storeNumber, owner, address }, key) => (
+                  <div key={key}>
+                    <div className={styles.item}>
+                      <p>이름</p>
+                      <Input inputName="name" setValue={name} />
+                    </div>
+                    <div className={styles.item}>
+                      <p>사업자 번호</p>
+                      <Input inputName="name" setValue={storeNumber} />
+                    </div>
+                    <div className={styles.item}>
+                      <p>소유자 이름</p>
+                      <Input inputName="name" setValue={owner} />
+                    </div>
+                    <div className={styles.item}>
+                      <p>주소</p>
+                      <Input inputName="name" setValue={address} />
+                    </div>
                   </div>
-                ))
-              }
+                ))}
+              </div>
             </div>
             <div className={styles.buttonList}>
               <Button variant="contained">삭제</Button>
