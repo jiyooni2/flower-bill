2075dba7c2547217a37fe36490e50d94a1747141
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-02-24 17:13:34 +0900

    feat: update category page

--- src/renderer/pages/CategoryPage/CategoryPage.tsx
+++ src/renderer/pages/CategoryPage/CategoryPage.tsx
@@ -1,70 +1,85 @@
-import { ChangeEvent, useRef, useState } from 'react';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import Button from '@mui/material/Button';
 import styles from './CategoryPage.module.scss';
 import { TreeItem, TreeView } from '@mui/lab';
-// import { useRecoilState } from 'recoil';
-// import { categoryState } from 'renderer/recoil/states';
-// import { GetCategoryOutput } from 'main/category/dtos/get-category.dto';
-// import { Category } from 'main/category/entities/category.entity';
-import { Category } from '../../types/index';
+import { useRecoilState } from 'recoil';
+import { categoryState } from 'renderer/recoil/states';
+import { GetCategoryOutput } from 'main/category/dtos/get-category.dto';
+import { Category } from 'main/category/entities/category.entity';
 import { ChevronRight, ExpandMore, AddRounded, Delete } from '@mui/icons-material';
 import { Typography } from '@mui/material';
 
-const data: Category[] = [
-  {
-    name: '국산',
-    level: 1,
-    parentCategory: null,
-    childCategories: [
-      {
-        name: '장미과',
-        level: 2,
-        parentCategory: null,
-        childCategories: [
-          {
-            name: '빨간 장미',
-            level: 3,
-            parentCategory: null,
-            childCategories: null,
-            products: null,
-          }
-        ],
-        products: null,
-      },
-    ],
-    products: null,
-  },
-  {
-    name: '외국산',
-    level: 1,
-    parentCategory: null,
-    childCategories: null,
-    products: null,
-  }
-];
+// const data: Category[] = [
+//   {
+//     name: '국산',
+//     level: 1,
+//     parentCategory: null,
+//     parentCategoryId: null,
+//     childCategories: [
+//       {
+//         name: '장미과',
+//         level: 2,
+//         parentCategory: {
+//           name: '국산',
+//           level: 1,
+//           parentCategory: null,
+//           childCategories: null,
+//           products: null,
+//         },
+//         parentCategoryId: 1,
+//         childCategories: [
+//           {
+//             name: '빨간 장미',
+//             level: 3,
+//             parentCategory: {
+//               name: '장미과',
+//               level: 2,
+//               parentCategory: {
+//                 name: '국산',
+//                 level: 1,
+//                 parentCategory: null,
+//                 childCategories: null,
+//                 products: null,
+//               },
+//               parentCategoryId: null,
+//               childCategories: null,
+//               products: null,
+//             },
+//             childCategories: null,
+//             products: null,
+//           },
+//         ],
+//         products: null,
+//       },
+//     ],
+//     products: null,
+//   },
+// ];
 
 
 const CategoryPage = () => {
-  // const [categories, setCategories] = useRecoilState(categoryState);
+  const [categories, setCategories] = useRecoilState(categoryState);
 
-  // useEffect(() => {
-  //   window.electron.ipcRenderer.sendMessage('get-category', {});
-  //   window.electron.ipcRenderer.on(
-  //     'get-category',
-  //     (args: GetCategoryOutput) => {
-  //       setCategories(args.category as Category);
-  //     }
-  //   );
-  // }, []);
-
-  const [categories, setCategories] = useState<Category[]>(data);
+  // const [categories, setCategories] = useState<Category>(data);
   const [keyword, setKeyWord] = useState<string>('');
+  const [clicked, setClicked] = useState<boolean>(false);
   const [categoryName, setCategoryName] = useState<string>('');
+  const [levelName, setLevelName] = useState<string>('');
   const [parentCategoryName, setParentCategoryName] = useState<string>('');
   const [parentCategoryId, setParentCategoryId] = useState<number>(0);
-  const [levelName, setLevelName] = useState<string>('');
-
   const nameInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    window.electron.ipcRenderer.sendMessage('get-category', {});
+    window.electron.ipcRenderer.on(
+      'get-category',
+      (args: GetCategoryOutput) => {
+        setCategories(args.category as Category);
+      }
+    );
+    console.log(categories);
+
+  }, []);
 
   const filter = (e: ChangeEvent<HTMLInputElement>) => {
     const word = e.target.value;
@@ -80,19 +95,49 @@ const CategoryPage = () => {
   };
 
 
-  const clickAddHandler = (item: Category) => {
-    if (!item) setLevelName('대분류')
-    else if (item.level === 1) setLevelName('중분류')
-    else if (item.level === 2) setLevelName('소분류')
+  const clickAddHandler = (item: Category, name: string) => {
+    if (name === "add"){
+      setClicked(false);
+      if (!item) setLevelName('대분류')
+      else if (item.level === 1) setLevelName('중분류')
+      else if (item.level === 2) setLevelName('소분류')
 
-    if (nameInputRef.current !== null) nameInputRef.current.focus();
-    if (item) {setParentCategoryName(item.name); setParentCategoryId(item.id) }
-    else {setParentCategoryName(''); setParentCategoryId(null)}
+      if (nameInputRef.current !== null) nameInputRef.current.focus();
+      if (item) {setParentCategoryName(item.name); setParentCategoryId(item.id) }
+      else {setParentCategoryName(''); setParentCategoryId(null)}
+    } else if (name === "item") {
+      setClicked(true);
+
+      if (item.level === 1) setLevelName('대분류');
+      else if (item.level === 2) setLevelName('중분류');
+      else if (!item) setLevelName('소분류');
+
+      if (item.parentCategory === null) {
+        setParentCategoryName('')
+      } else {
+        setParentCategoryName(item.parentCategory.name);
+      }
+      setCategoryName(item.name);
+    }
+
+
+    const newCategory = {
+      name: categoryName,
+      parentCategoryId: parentCategoryId,
+    };
+
+    window.electron.ipcRenderer.sendMessage('create-category', newCategory);
   };
 
 
   const clickDeleteHandler = (nodes: Category) => {
     console.log(nodes)
+
+    const delCategory = {
+      id: nodes.id
+    };
+
+    window.electron.ipcRenderer.sendMessage('delete-category', delCategory);
   }
 
 
@@ -109,6 +154,10 @@ const CategoryPage = () => {
     setParentCategoryName('');
   };
 
+  // const updateCategoryHandler = () => {
+
+  // };
+
 
   const addTreeItem = (item: Category, text: string) => {
     return (
@@ -118,7 +167,7 @@ const CategoryPage = () => {
         nodeId={`${item.name}${Math.random()}`}
         icon={<AddRounded />}
         sx={{ marginTop: '15px' }}
-        onClick={() => clickAddHandler(item)}
+        onClick={() => clickAddHandler(item, 'add')}
       />
     )
   }
@@ -157,6 +206,7 @@ const CategoryPage = () => {
           />
         </div>
       }
+      onClick={() => clickAddHandler(nodes, 'item')}
       sx={{ marginTop: '15px' }}
     >
       {Array.isArray(nodes.childCategories)
@@ -164,7 +214,9 @@ const CategoryPage = () => {
         : null}
       {nodes.childCategories
         ? nodes.childCategories.map(() => addTree(nodes, true))
-        : nodes.level < 3 ? addTree(nodes, false) : null}
+        : nodes.level < 3
+        ? addTree(nodes, false)
+        : null}
     </TreeItem>
   );
 
@@ -214,7 +266,7 @@ const CategoryPage = () => {
               nodeId={Math.random().toString()}
               icon={<AddRounded />}
               sx={{ marginTop: '15px' }}
-              onClick={() => clickAddHandler(null)}
+              onClick={() => clickAddHandler(null, 'add')}
             />
           </TreeView>
         </div>
@@ -265,14 +317,26 @@ const CategoryPage = () => {
               </div>
               <div className={styles.buttonList}>
                 <div></div>
-                <Button
-                  variant="contained"
-                  size="small"
-                  sx={{ marginRight: '10px' }}
-                  onClick={newCategoryHandler}
-                >
-                  생성
-                </Button> </div>
+                {!clicked ? (
+                  <Button
+                    variant="contained"
+                    size="small"
+                    sx={{ marginRight: '10px' }}
+                    onClick={newCategoryHandler}
+                  >
+                    생성
+                  </Button>
+                ) : (
+                  <Button
+                    variant="contained"
+                    size="small"
+                    sx={{ marginRight: '10px', backgroundColor: 'coral' }}
+                    // onClick={updateCategoryHandler}
+                  >
+                    수정
+                  </Button>
+                )}
+              </div>
             </div>
           </div>
         </div>
