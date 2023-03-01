3140465f3d19525a6e591e70f5f520c957a94987
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-03-01 16:17:52 +0900

    feat: update bill modal

--- src/renderer/pages/BillPage/components/BillModal/BillModal.tsx
+++ src/renderer/pages/BillPage/components/BillModal/BillModal.tsx
@@ -2,8 +2,12 @@ import { Button } from '@mui/material';
 // import { OrderProduct } from 'main/orderProduct/entities/orderProduct.entity';
 import styles from './BillModal.module.scss';
 import { useRecoilValue } from 'recoil';
-import { memoState, orderProductsState, storeState } from 'renderer/recoil/states';
+import { businessState, memoState, orderProductsState, storeState, tokenState } from 'renderer/recoil/states';
 import Modal from './Modal';
+import { useRef } from 'react';
+import { CreateBillInput, CreateBillOutput } from 'main/bill/dtos/create-bill.dto';
+import { GetBillOutput } from 'main/bill/dtos/get-bill.dto';
+import { Printer } from 'react-thermal-printer';
 
 interface IProps {
   isOpen: boolean;
@@ -11,12 +15,15 @@ interface IProps {
 }
 
 const BillModal = ({ isOpen, setIsOpen }: IProps) => {
+  const business = useRecoilValue(businessState);
+  const token = useRecoilValue(tokenState);
   const orderProducts = useRecoilValue(orderProductsState);
   const memo = useRecoilValue(memoState);
   const store = useRecoilValue(storeState);
+  const printRef = useRef();
 
 
-  const handleClick = () => {
+  const handleClick = async () => {
     setIsOpen(false);
     const orderProductInputs = orderProducts.map((orderProduct) => ({
       count: orderProduct.count,
@@ -24,13 +31,27 @@ const BillModal = ({ isOpen, setIsOpen }: IProps) => {
       orderPrice: orderProduct.orderPrice,
     }));
 
-    const bill = {
+    const newBill: CreateBillInput = {
+      businessId: business.id,
+      token,
       storeId: store.id,
       memo,
       orderProductInputs,
     };
 
-    window.electron.ipcRenderer.sendMessage('create-bill', bill);
+    window.electron.ipcRenderer.sendMessage('create-bill', {
+      ...newBill,
+    });
+    window.electron.ipcRenderer.on(
+      'create-bill',
+      ({ ok, error, bill }: GetBillOutput) => {
+        if (ok) {
+          console.log(bill)
+        } else if (error) {
+          console.log(error);
+        }
+      }
+    );
   };
 
   let sum = 0;
@@ -45,124 +66,124 @@ const BillModal = ({ isOpen, setIsOpen }: IProps) => {
 
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-      <div style={{ height: '490px'}}>
-        <table
-          style={{ width: '100%', border: '0' }}
-          cellPadding="0"
-          cellSpacing="0"
-          className="title"
-        >
-          <tr>
-            <td align="center">
-              <span style={{ fontSize: '22px', fontWeight: 'bold' }}>
-                영&ensp;수&ensp;증
-              </span>
-            </td>
-          </tr>
+        <div ref={printRef} style={{ height: '375px' }}>
+          <table
+            style={{ width: '100%', border: '0' }}
+            cellPadding="0"
+            cellSpacing="0"
+            className="title"
+          >
+            <tr>
+              <td align="center">
+                <span style={{ fontSize: '22px', fontWeight: 'bold' }}>
+                  영&ensp;수&ensp;증
+                </span>
+              </td>
+            </tr>
 
-          <br />
-        </table>
-        <table
-          style={{ width: '100%', border: '0' }}
-          cellPadding="0"
-          cellSpacing="0"
-          className={styles.ownerData}
-        >
-          <tr>
-            <td style={{ width: '45%' }}>
-              <span style={{ fontSize: '10px', fontWeight: '400' }}>
-                {' '}
-                (공급받는자용)
-              </span>
-            </td>
-            <td className={styles.name}>{/* 사업자 이름*/} ㅁㄴㅇ 님</td>
-            <td className={styles.for}>귀하</td>
-          </tr>
-        </table>
-        <table
-          style={{ width: '100%' }}
-          cellPadding="0"
-          cellSpacing="0"
-          className={`${styles.tbl} ${styles.stamp}`}
-        >
-          <tr>
-            <td
-              style={{ width: '7%' }}
-              rowSpan={5}
-              align="center"
-              className={styles.owner}
-            >
-              공 급 자
-            </td>
-            <th style={{ width: '18%' }}>
-              사업자
-              <br />
-              등록번호
-            </th>
-            <td colSpan={3} className={styles.businessNumber}>
-              {/* 사업자 번호 */}
-            </td>
-          </tr>
-          <tr>
-            <th>상호</th>
-            <td style={{ width: '25%' }} align="center">
-              {/* 상호 */}
-            </td>
-            <th style={{ width: '14%' }}>성명</th>
-            <td style={{ width: '20%' }} align="center">
-              {/* owner name */}
-            </td>
-          </tr>
-          <tr>
-            <th>
-              사업자
-              <br />
-              소재지
-            </th>
-            <td colSpan={3} style={{ fontSize: '15px', textAlign: 'center' }}>
-              {/* 가게 주소 */}
-            </td>
-          </tr>
-          <tr>
-            <th>업태</th>
-            <td align="center">{/* 업태 */}</td>
-            <th>종목</th>
-            <td align="center">{/* 업종 */}</td>
-          </tr>
-        </table>
-        <table
-          width="100%"
-          cellSpacing="0"
-          cellPadding="0"
-          className={styles.tbl}
-        >
-          <tr>
-            <th>작성년월일</th>
-            <th>공급대가총액</th>
-            <th>비고</th>
-          </tr>
-          <tr>
-            <td className={styles.date}>{`${year} . ${month} . ${day}`}</td>
-            <td className={styles.total}>
-              <span style={{ fontWeight: 'bold' }}>₩</span>{' '}
-              {sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-            </td>
-            <td> </td>
-          </tr>
-        </table>
-        <table
-          width="100%"
-          cellSpacing="0"
-          cellPadding="0"
-          className={styles.tbl}
-        >
-          <tr>
-            <th>월일</th>
-            <th>품목</th>
-            <th>수량</th>
-            <th>단가</th>
-            <th>금액</th>
-          </tr>
+            <br />
+          </table>
+          <table
+            style={{ width: '100%', border: '0' }}
+            cellPadding="0"
+            cellSpacing="0"
+            className={styles.ownerData}
+          >
+            <tr>
+              <td style={{ width: '45%' }}>
+                <span style={{ fontSize: '10px', fontWeight: '400' }}>
+                  {' '}
+                  (공급받는자용)
+                </span>
+              </td>
+              <td className={styles.name}>{/* 구매자 */} 님</td>
+              <td className={styles.for}>&ensp;귀하</td>
+            </tr>
+          </table>
+          <table
+            style={{ width: '100%' }}
+            cellPadding="0"
+            cellSpacing="0"
+            className={`${styles.tbl} ${styles.stamp}`}
+          >
+            <tr>
+              <td
+                style={{ width: '7%' }}
+                rowSpan={5}
+                align="center"
+                className={styles.owner}
+              >
+                공 급 자
+              </td>
+              <th style={{ width: '18%' }}>
+                사업자
+                <br />
+                등록번호
+              </th>
+              <td colSpan={3} className={styles.businessNumberDiv}>
+                {business.businessNumber}
+              </td>
+            </tr>
+            <tr>
+              <th>상호</th>
+              <td style={{ width: '25%', fontSize: '15px' }} align="center">
+                {business.name}
+              </td>
+              <th style={{ width: '14%' }}>성명</th>
+              <td style={{ width: '20%', fontSize: '15px' }} align="center">
+                {business.businessOwnerName}
+              </td>
+            </tr>
+            <tr>
+              <th>
+                사업자
+                <br />
+                소재지
+              </th>
+              <td colSpan={3} style={{ fontSize: '15px', textAlign: 'center' }}>
+                {business.address}
+              </td>
+            </tr>
+            <tr>
+              <th>업태</th>
+              <td align="center">{/* 업태 */}</td>
+              <th>종목</th>
+              <td align="center">{/* 업종 */}</td>
+            </tr>
+          </table>
+          <table
+            width="100%"
+            cellSpacing="0"
+            cellPadding="0"
+            className={styles.tbl}
+          >
+            <tr>
+              <th>작성년월일</th>
+              <th>공급대가총액</th>
+              <th>비고</th>
+            </tr>
+            <tr>
+              <td className={styles.date}>{`${year} . ${month} . ${day}`}</td>
+              <td className={styles.total}>
+                <span style={{ fontWeight: 'bold' }}>₩</span>{' '}
+                {sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+              </td>
+              <td> </td>
+            </tr>
+          </table>
+          <table
+            width="100%"
+            cellSpacing="0"
+            cellPadding="0"
+            className={styles.tbl}
+          >
+            <tr>
+              <th>월일</th>
+              <th>품목</th>
+              <th>수량</th>
+              <th>단가</th>
+              <th>금액</th>
+            </tr>
             {orderProducts.map((orderProduct) => {
               return (
                 <tr key={orderProduct.id}>
@@ -174,14 +195,14 @@ const BillModal = ({ isOpen, setIsOpen }: IProps) => {
                 </tr>
               );
             })}
-        </table>
-        <div className={styles.sumDiv}>
-          <td className={styles.lastSum}>합&ensp;&ensp;계</td>
-          <td style={{ float: 'right', marginRight: '10px', color: 'black' }}>
-            ₩ {sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-          </td>
+          </table>
+          <div className={styles.sumDiv}>
+            <td className={styles.lastSum}>합&ensp;&ensp;계</td>
+            <td style={{ float: 'right', marginRight: '10px', color: 'black' }}>
+              ₩ {sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            </td>
+          </div>
         </div>
-      </div>
       <div>
         <Button
           variant="contained"
