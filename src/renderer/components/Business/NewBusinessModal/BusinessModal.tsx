899589018be250b6b439df91cd09bce3615810b9
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-03-01 13:40:10 +0900

    feat: add create business api to page

--- src/renderer/components/Business/NewBusinessModal/BusinessModal.tsx
+++ src/renderer/components/Business/NewBusinessModal/BusinessModal.tsx
@@ -1,49 +1,82 @@
+import { CreateBusinessOutput } from 'main/business/dtos/create-business.dto';
 import Modal from './Modal';
 import { Button, TextField, Typography } from '@mui/material';
-import { useRecoilState } from 'recoil';
-import useInputs from 'renderer/hooks/useInputs';
-import { businessState } from 'renderer/recoil/states';
-import { CreateStore } from 'renderer/types';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { businessState, businessesState, tokenState } from 'renderer/recoil/states';
+import { Business } from 'main/business/entities/business.entity';
+import { useState } from 'react';
+import { GetBusinessesOutput } from 'main/business/dtos/get-businesses.dto';
 
 interface IProps {
   isOpen: boolean;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
-//name' | 'businessNumber' | 'businessOwnerName' | 'address
-const BusinessModal = ({ isOpen, setIsOpen }: IProps) => {
-  const [currentBusiness, setCurrentBusiness] = useRecoilState(businessState);
-  const [{ businessNumber, address, name, owner }, handleChange] =
-    useInputs<CreateStore>({
-      businessNumber: '',
-      address: '',
-      name: '',
-      owner: '',
-    });
 
+const BusinessModal = ({ isOpen, setIsOpen }: IProps) => {
+  const business = useRecoilValue(businessState);
+  const token = useRecoilValue(tokenState);
+  const [businesses, setBusinesses] = useRecoilState(businessesState);
+  const [businessNumber, setBusinessNumber] = useState<number>(0);
+  const [address, setAddress] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [owner, setOwner] = useState<string>('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>, dataName: string) => {
+    const { value } = event.target;
+    if (dataName === 'owner')setOwner(value)
+    else if (dataName === 'name') setName(value)
+    else if (dataName === 'number') setBusinessNumber(Number(value))
+    else setAddress(value)
+  }
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     const newBusiness = {
       name,
-      businessNumber: BigInt(businessNumber),
+      businessNumber: businessNumber,
       businessOwnerName: owner,
       address,
     };
 
-    if (name == '' || businessNumber == '' || owner == '' || name == '') {
+    if (name == '' || businessNumber == 0 || owner == '' || name == '') {
       window.alert('사업자를 생성할 수 없습니다.\n모든 입력 사항을 작성해주시기 바랍니다.')
     } else {
       if (window.confirm('정말 생성하시겠습니까?')){
-        window.electron.ipcRenderer.sendMessage('create-business', newBusiness);
-        setCurrentBusiness(newBusiness)
         console.log(newBusiness)
+        window.electron.ipcRenderer.sendMessage('create-business', {
+          ...newBusiness,
+          token,
+          businessId: business.id,
+        });
+
+        window.electron.ipcRenderer.on(
+          'create-business',
+          ({ ok, error }: CreateBusinessOutput) => {
+            if (ok) {
+              window.electron.ipcRenderer.sendMessage('get-businesses', {
+                token,
+                businessId: business.id,
+              });
+              window.electron.ipcRenderer.on(
+                'get-businesses',
+                (args: GetBusinessesOutput) => {
+                  setBusinesses(args.businesses as Business[]);
+                }
+              );
+            } else if (error) {
+              console.log(error);
+            }
+          }
+        );
+        setBusinessNumber(0)
+        setName('')
+        setAddress('')
+        setOwner('')
         setIsOpen(false);
       }
     }
   };
-
-  console.log(currentBusiness);
 
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
@@ -64,7 +97,9 @@ const BusinessModal = ({ isOpen, setIsOpen }: IProps) => {
               label="사업자등록번호"
               name="businessNumber"
               variant="filled"
-              onChange={handleChange}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                handleChange(event, 'number')
+              }
               value={businessNumber}
             />
           </div>
@@ -74,7 +109,9 @@ const BusinessModal = ({ isOpen, setIsOpen }: IProps) => {
               label="상호"
               name="name"
               variant="filled"
-              onChange={handleChange}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                handleChange(event, 'name')
+              }
               value={name}
             />
           </div>
@@ -84,7 +121,9 @@ const BusinessModal = ({ isOpen, setIsOpen }: IProps) => {
               label="성명"
               name="owner"
               variant="filled"
-              onChange={handleChange}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                handleChange(event, 'owner')
+              }
               value={owner}
             />
           </div>
@@ -94,7 +133,9 @@ const BusinessModal = ({ isOpen, setIsOpen }: IProps) => {
               label="사업장 소재지(선택)"
               name="address"
               variant="filled"
-              onChange={handleChange}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                handleChange(event, 'address')
+              }
               value={address}
             />
           </div>
