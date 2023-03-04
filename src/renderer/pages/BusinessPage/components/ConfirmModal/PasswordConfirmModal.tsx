5514cae1a56fbce13760f6304225bf267ea7d250
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-03-05 00:59:11 +0900

    feat: add password confirm modal

--- src/renderer/pages/BusinessPage/components/ConfirmModal/PasswordConfirmModal.tsx
+++ src/renderer/pages/BusinessPage/components/ConfirmModal/PasswordConfirmModal.tsx
@@ -1,63 +1,68 @@
-import { Button } from '@mui/material';
 import Modal from './Modal';
-import { useRecoilState } from 'recoil';
-import useInputs from 'renderer/hooks/useInputs';
-import { businessState } from 'renderer/recoil/states';
-import { CreateStore } from 'renderer/types';
-import { useRef, useState } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { businessState, businessesState, tokenState } from 'renderer/recoil/states';
+import { useState } from 'react';
 import styles from './PasswordConfirmModal.module.scss'
+import { DeleteBusinessOutput } from 'main/business/dtos/delete-business.dto';
+import { CheckPasswordInput, CheckPasswordOutput } from 'auth/dtos/check-password.dto';
 
 interface IProps {
   isOpen: boolean;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  purpose: string;
 }
-//name' | 'businessNumber' | 'businessOwnerName' | 'address
+
 const PasswordConfirmModal = ({ isOpen, setIsOpen }: IProps) => {
-  const [currentBusiness, setCurrentBusiness] = useRecoilState(businessState);
-  const [pw, setPW] = useState<string>('');
-  const [hasError, setHasError] = useState(false);
-  const [{ businessNumber, address, name, owner }, handleChange] =
-    useInputs<CreateStore>({
-      businessNumber: '',
-      address: '',
-      name: '',
-      owner: '',
+  const token = useRecoilValue(tokenState);
+  const businesses = useRecoilValue(businessesState);
+  const [business, setBusiness] = useRecoilState(businessState);
+  const [password, setPassword] = useState<string>('');
+
+  const clickHandler = () => {
+    const check: CheckPasswordInput = {
+      password,
+      token,
+    };
+
+    window.electron.ipcRenderer.sendMessage('check-password', {
+      ...check,
+      businessId: business.id,
     });
 
-  // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-  //   event.preventDefault();
+    window.electron.ipcRenderer.on(
+      'check-password',
+      ({ ok, error }: CheckPasswordOutput) => {
+        if (ok) {
+          window.electron.ipcRenderer.sendMessage('delete-business', {
+            businessId: business.id,
+            token,
+          });
 
-  //   const newBusiness = {
-  //     name,
-  //     businessNumber: BigInt(businessNumber),
-  //     businessOwnerName: owner,
-  //     address,
-  //   };
-
-  //   if (name == '' || businessNumber == '' || owner == '' || name == '') {
-  //     window.alert(
-  //       '사업자를 생성할 수 없습니다.\n모든 입력 사항을 작성해주시기 바랍니다.'
-  //     );
-  //   } else {
-  //     if (window.confirm('정말 생성하시겠습니까?')) {
-  //       window.electron.ipcRenderer.sendMessage('create-business', newBusiness);
-  //       setCurrentBusiness(newBusiness);
-  //       console.log(newBusiness);
-  //       setIsOpen(false);
-  //     }
-  //   }
-  // };
-
-  const keyPressHandler = (event: React.KeyboardEvent<HTMLElement>) => {
-    if (event.key === 'Enter' && event.nativeEvent.isComposing){
-      // if (pw !== ) {
-        // setHasError(true)
-      // }
-    }
-  };
+          window.electron.ipcRenderer.on(
+            'delete-business',
+            ({ ok, error }: DeleteBusinessOutput) => {
+              if (ok) {
+                setBusiness(businesses[0])
+                setIsOpen(false);
+                window.location.reload();
+              }
+              if (error) {
+                console.log(error);
+                window.alert('비밀번호가 일치하지 않습니다.');
+                setIsOpen(false);
+              }
+            }
+          );
+        }
+        if (error) {
+          console.log(error);
+        }
+      }
+    );
+  }
 
   const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPW(event.target.value)
+    setPassword(event.target.value);
   };
 
   return (
@@ -66,13 +71,20 @@ const PasswordConfirmModal = ({ isOpen, setIsOpen }: IProps) => {
         <div>
           <p className={styles.label}>비밀번호를 입력해주십시오.</p>
           <p className={styles.pwContainer}>
-            <input type="password" className={styles.pwInput} onKeyDown={keyPressHandler} onChange={changeHandler} />
+            <input
+              type="password"
+              className={styles.pwInput}
+              onChange={changeHandler}
+            />
           </p>
         </div>
-        {hasError && <p className={styles.errorMessage}>비밀번호가 일치하지 않습니다.</p>}
-        {/* <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <Button onClick={handleClick} variant='contained' size='small' sx={{ width: '50%', marginTop: '23px', marginLeft: '17px'}}>확인</Button>
-        </div> */}
+        <button
+          className={styles.deleteButton}
+          disabled={!password ? true : false}
+          onClick={clickHandler}
+        >
+          확인
+        </button>
       </div>
     </Modal>
   );
