ac4c96e2cecca3ab9b9ef79479155d2890679af7
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-03-05 18:32:17 +0900

    feat: update discount modal

--- src/renderer/pages/BillPage/components/DiscountModal/DiscountModal.tsx
+++ src/renderer/pages/BillPage/components/DiscountModal/DiscountModal.tsx
@@ -1,10 +1,12 @@
-import { TextField, Typography } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from '@mui/material';
 import styles from './DiscountModal.module.scss';
 import Button from '@mui/material/Button';
 // import { useRecoilState } from 'recoil';
 // import { memoState } from 'renderer/recoil/states';
 import { useState } from 'react';
 import MiniModal from './MiniModal';
+import { useRecoilValue } from 'recoil';
+import { orderProductsState } from 'renderer/recoil/states';
 
 interface IProps {
   isOpen: boolean;
@@ -12,8 +14,10 @@ interface IProps {
 }
 
 const DiscountModal = ({ isOpen, setIsOpen }: IProps) => {
-  // const [memo, setMemo] = useRecoilState(memoState);
+  const orderProduct = useRecoilValue(orderProductsState);
   const [discount, setDiscount] = useState('');
+
+  console.log(orderProduct);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setDiscount(event.target.value);
@@ -25,10 +29,10 @@ const DiscountModal = ({ isOpen, setIsOpen }: IProps) => {
 
   return (
     <MiniModal isOpen={isOpen} setIsOpen={setIsOpen}>
-      <Typography variant="h6" sx={{ margin: '15px auto' }}>
+      <Typography variant="h6" sx={{ margin: '15px auto 10px auto' }}>
         할인 추가하기
       </Typography>
-      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', marginLeft: '-50px' }}>
+      {/* <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', marginLeft: '-50px' }}>
         <TextField
           placeholder="할인율"
           multiline
@@ -37,14 +41,45 @@ const DiscountModal = ({ isOpen, setIsOpen }: IProps) => {
           onChange={handleChange}
         />
         <Typography variant="h6" sx={{ marginTop: '10px'}}>%</Typography>
+      </div> */}
+      <div style={{ width: '90%', margin: '0 auto' }}>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow
+                sx={{ fontWeight: 'bold', borderBottom: '1.5px solid' }}
+              >
+                <TableCell size="small">상품</TableCell>
+                <TableCell size="small">금액</TableCell>
+                <TableCell size="small">할인</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {orderProduct.map((item) => (
+                <TableRow key={item.id}>
+                  <TableCell size="small">{item.product.name}</TableCell>
+                  <TableCell size="small">{item.product.price} 원</TableCell>
+                  <TableCell
+                    size="small"
+                    sx={{ display: 'flex', flexDirection: 'row' }}
+                  >
+                    <input className={styles.dataInput} />
+                    <span>%</span>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
-      <Button
+      <div></div>
+      {/* <Button
         variant="contained"
         onClick={handleClick}
         sx={{ width: '100px', margin: '0 auto' }}
       >
         확인
-      </Button>
+      </Button> */}
     </MiniModal>
   );
 };
