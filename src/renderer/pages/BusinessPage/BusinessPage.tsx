eb128590d0d711115276a36adba52177db72661b
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-03-25 00:29:08 +0900

    feat: change to use modal

--- src/renderer/pages/BusinessPage/BusinessPage.tsx
+++ src/renderer/pages/BusinessPage/BusinessPage.tsx
@@ -27,7 +27,7 @@ const BuisnessPage = () => {
   const [name, setName] = useState<string>('');
   const [businessOwnerName, setBusinessOwnerName] = useState<string>('');
   const [address, setAddress] = useState<string>('');
-  // const [isOpen, setIsOpen] = useState<boolean>(true);
+  const [isOpen, setIsOpen] = useState<boolean>(true);
 
   useEffect(() => {
     setBusinessNumber(business.businessNumber.toString());
@@ -114,7 +114,7 @@ const BuisnessPage = () => {
   ) => {
     const value = event.target.value;
 
-    if (dataName === 'number') setBusinessNumber(parseInt(value));
+    if (dataName === 'number') setBusinessNumber(value);
     else if (dataName === 'name') setName(value);
     else if (dataName === 'owner') setBusinessOwnerName(value);
     else if (dataName === 'address') setAddress(value);
@@ -122,82 +122,84 @@ const BuisnessPage = () => {
 
   return (
     <>
-      {/* <PasswordConfirmModal isOpen={isOpen} setIsOpen={setIsOpen} /> */}
-      <div className={styles.container}>
-        <div className={styles.content}>
-          <div className={styles.infoContent}>
-            <Typography
-              variant="h6"
-              fontWeight={600}
-              sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                fontSize: '24px',
-                marginTop: '20px',
-              }}
-            >
-              사업자 정보
-            </Typography>
-            <div className={styles.list}>
-              <div>
+      {isOpen && <PasswordConfirmModal isOpen={isOpen} setIsOpen={setIsOpen} />}
+      {!isOpen && (
+        <div className={styles.container}>
+          <div className={styles.content}>
+            <div className={styles.infoContent}>
+              <Typography
+                variant="h6"
+                fontWeight={600}
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  fontSize: '24px',
+                  marginTop: '20px',
+                }}
+              >
+                사업자 정보
+              </Typography>
+              <div className={styles.list}>
                 <div>
-                  <div className={styles.item}>
-                    <p className={styles.labels}>사업자 번호</p>
-                    <input
-                      value={businessNumber}
-                      className={styles.dataInput}
-                      onChange={(event) => changeHandler(event, 'number')}
-                    />
-                  </div>
-                  <div className={styles.item}>
-                    <p className={styles.labels}>사업장 이름</p>
-                    <input
-                      value={name}
-                      className={styles.dataInput}
-                      onChange={(event) => changeHandler(event, 'name')}
-                    />
-                  </div>
-                  <div className={styles.item}>
-                    <p className={styles.labels}>소유자 이름</p>
-                    <input
-                      value={businessOwnerName}
-                      className={styles.dataInput}
-                      onChange={(event) => changeHandler(event, 'owner')}
-                    />
-                  </div>
-                  <div className={styles.item}>
-                    <p className={styles.labels}>사업장 주소</p>
-                    <input
-                      value={address}
-                      className={styles.dataInput}
-                      onChange={(event) => changeHandler(event, 'address')}
-                    />
+                  <div>
+                    <div className={styles.item}>
+                      <p className={styles.labels}>사업자 번호</p>
+                      <input
+                        value={businessNumber}
+                        className={styles.dataInput}
+                        onChange={(event) => changeHandler(event, 'number')}
+                      />
+                    </div>
+                    <div className={styles.item}>
+                      <p className={styles.labels}>사업장 이름</p>
+                      <input
+                        value={name}
+                        className={styles.dataInput}
+                        onChange={(event) => changeHandler(event, 'name')}
+                      />
+                    </div>
+                    <div className={styles.item}>
+                      <p className={styles.labels}>소유자 이름</p>
+                      <input
+                        value={businessOwnerName}
+                        className={styles.dataInput}
+                        onChange={(event) => changeHandler(event, 'owner')}
+                      />
+                    </div>
+                    <div className={styles.item}>
+                      <p className={styles.labels}>사업장 주소</p>
+                      <input
+                        value={address}
+                        className={styles.dataInput}
+                        onChange={(event) => changeHandler(event, 'address')}
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className={styles.buttonList}>
-                <Button
-                  variant="contained"
-                  size="small"
-                  sx={{ marginLeft: '40px' }}
-                  color="error"
-                  onClick={deleteDataHandler}
-                >
-                  삭제
-                </Button>
-                <Button
-                  variant="contained"
-                  size="small"
-                  sx={{ marginRight: '10px' }}
-                  onClick={updateDataHandler}
-                >
-                  수정
-                </Button>
+                <div className={styles.buttonList}>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    sx={{ marginLeft: '40px' }}
+                    color="error"
+                    onClick={deleteDataHandler}
+                  >
+                    삭제
+                  </Button>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    sx={{ marginRight: '10px' }}
+                    onClick={updateDataHandler}
+                  >
+                    수정
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
