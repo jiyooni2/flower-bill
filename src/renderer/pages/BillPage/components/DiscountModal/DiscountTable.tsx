a6d5f46d9db0ceda6f2de73f65dd8421df13c8d7
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-03-25 19:01:22 +0900

    feat: add discount modal

---
+++ src/renderer/pages/BillPage/components/DiscountModal/DiscountTable.tsx
@@ -0,0 +1,68 @@
+import { TableCell, TableRow } from "@mui/material";
+import { OrderProduct } from "main/orderProduct/entities/orderProduct.entity";
+import styles from './DiscountTable.module.scss'
+import { useState } from "react";
+import { useRecoilState } from "recoil";
+import { orderProductsState } from "renderer/recoil/states";
+
+interface IProps {
+  orderProduct: OrderProduct;
+}
+
+const DiscountTable = ({orderProduct}: IProps) => {
+  const [discount, setDiscount] = useState<string>(orderProduct.orderPrice.toString());
+  const [orderProducts, setOrderProducts] = useRecoilState(orderProductsState)
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDiscount(event.target.value);
+  };
+
+  const updatePriceHandler = () => {
+    setOrderProducts(
+      orderProducts.map((item) => {
+        if (item.product.id === orderProduct.product.id) {
+          return {
+            ...item,
+            orderPrice: parseInt(discount),
+          };
+        }
+
+        return item;
+      })
+    );
+  };
+
+  return (
+    <>
+      <TableRow>
+        <TableCell size="small">{orderProduct.product.name}</TableCell>
+        <TableCell size="small" style={{ width: '25%'}}>{orderProduct.product.price}</TableCell>
+        <TableCell
+          size="small"
+          align="center"
+        >
+          <input
+            className={styles.dataInput}
+            value={discount}
+            onChange={handleChange}
+          />
+          <span style={{ marginTop: '4px', marginLeft: '5px' }}>원</span>
+        </TableCell>
+        <TableCell
+          size="small"
+          sx={{
+            width: '23%',
+            color: 'royalblue',
+            cursor: 'pointer',
+            fontWeight: '500',
+            '&:hover': { color: '#228bf2' },
+          }}
+        >
+          <span onClick={updatePriceHandler}>저장</span>
+        </TableCell>
+      </TableRow>
+    </>
+  );
+};
+
+export default DiscountTable;
