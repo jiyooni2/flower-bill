bbb9a8a3e0f491da945c36d0b7227a2cb3fe36f1
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-04-25 22:46:16 +0900

    refactor: refactor code and modify styles

---
+++ src/renderer/pages/BillsPage/UpdateBillPage/Buttons.tsx
@@ -0,0 +1,101 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+
+type IProps = {
+  setIsSearchStoreOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsDiscountOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsMemoOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsBillOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Buttons = ({ setIsSearchStoreOpen, setIsDiscountOpen, setIsMemoOpen, setIsBillOpen } : IProps) => {
+  const billClickHandler = () => {
+    setIsBillOpen(true);
+  };
+
+  return (
+    <>
+      <div
+            style={{
+              marginTop: '15px',
+              display: 'flex',
+              justifyContent: 'center',
+              width: '100%',
+              gap: '10px',
+            }}
+          >
+            <Button
+              variant="contained"
+              sx={{
+                width: '40%',
+                height: '33px',
+                backgroundColor: 'ghostwhite',
+                opacity: '0.9',
+                marginleft: '20px',
+                color: '#228af2',
+                '&:hover': {
+                  background: '#6b5fb9',
+                  opacity: '0.9',
+                  color: 'lightskyblue',
+                },
+              }}
+              onClick={() => setIsSearchStoreOpen(true)}
+            >
+              판매처
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => setIsDiscountOpen(true)}
+              sx={{
+                width: '40%',
+                height: '33px',
+                backgroundColor: 'ghostwhite',
+                color: '#228af2',
+                '&:hover': {
+                  background: '#6b5fb9',
+                  opacity: '0.9',
+                  color: 'lightskyblue',
+                },
+              }}
+            >
+              판매가
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => setIsMemoOpen(true)}
+              sx={{
+                width: '40%',
+                height: '33px',
+                backgroundColor: 'ghostwhite',
+                color: '#228af2',
+                '&:hover': {
+                  background: '#6b5fb9',
+                  opacity: '0.9',
+                  color: 'lightskyblue',
+                },
+              }}
+            >
+              메모
+            </Button>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <Button
+              variant="contained"
+              onClick={billClickHandler}
+              sx={{
+                height: '33px',
+                width: '100%',
+                marginTop: '10px',
+                backgroundColor: '#228bf2',
+                color: '#e8f8e2',
+              }}
+            >
+              계산서 수정하기
+            </Button>
+          </div>
+    </>
+  )
+};
+
+export default Buttons;
---
+++ src/renderer/pages/BillsPage/UpdateBillPage/UpdateBillSum.tsx
@@ -0,0 +1,48 @@
+import { OrderProduct } from "main/orderProduct/entities/orderProduct.entity";
+import useAddComma from "renderer/hooks/useAddComma";
+import styles from './UpdateSum.module.scss'
+
+type IProps = {
+  orderProducts: OrderProduct[];
+}
+
+const UpdateBillSum = ({ orderProducts }: IProps) => {
+  const addComma = useAddComma();
+
+  let sum = 0;
+  orderProducts?.map((items) => {
+    sum += items.orderPrice * items.count;
+  });
+
+  const discount = 0;
+
+  return (
+    <div>
+            <div className={styles.total}>
+              <p className={styles.totalName}>과세&nbsp;물품</p>
+              <h6 className={styles.totalNum}>
+                {addComma(Math.round(sum / 1.1).toString())} 원
+              </h6>
+            </div>
+            <hr />
+            <div className={styles.total}>
+              <p className={styles.totalName}>부&nbsp;&nbsp;가&nbsp;&nbsp;세</p>
+              <h6 className={styles.totalNum}>
+                {addComma(Math.round(Math.round(sum / 1.1) * 0.1).toString())} 원
+              </h6>
+            </div>
+            <hr />
+            <div className={styles.total}>
+              <p className={styles.totalName}>
+                합&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;계
+              </p>
+              <p className={styles.totalNum}>
+                {addComma((sum - (sum * discount) / 100).toString())} 원
+              </p>
+            </div>
+            <hr />
+          </div>
+  )
+};
+
+export default UpdateBillSum;
